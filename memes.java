c3706b50b77d26d3c46b73a232c1107fbaa5efd8
David Collins <dave847@gmail.com>
2016-10-11 20:50:15 -0400

    Update memes.java

--- memes.java
+++ memes.java
@@ -1,5 +1,9 @@
 public class memefiend extends meme {
-for(int i = 0; i < 69; i++) {
-	System.out.println("meme);
+	public int memes;
+	public memefiend(int memes) {
+		this.memes = memes;
+	}
+	for(int i = 0; i < 69; i++) {
+		System.out.println("meme);
+	}
 }
-			   }
